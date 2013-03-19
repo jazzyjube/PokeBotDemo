37e6d8eaaf9b8c2383bae5f5ad3cbee30af6f198
PowPo <you@example.com>
2013-03-19 12:09:35 +0100

    Ajout de la persistance Remy2

---
+++ src/main/java/fr/univaix/iut/pokebattle/App.java
@@ -0,0 +1,21 @@
+package fr.univaix.iut.pokebattle;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Initializes the Entity manager
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pokebattle");
+        EntityManager em = emf.createEntityManager();
+
+        Pokemon pika = em.find(Pokemon.class, "PIKACHU");
+        System.out.println("\n" + pika.getNomP() + " appartient a " + pika.getNomD());
+        em.close();
+        emf.close();
+    }
+}
