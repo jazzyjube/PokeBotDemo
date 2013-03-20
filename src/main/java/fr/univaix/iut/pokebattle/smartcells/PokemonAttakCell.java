04d96378f461fba4b95a20c944003ba0aa218c58
jazzyjube <julienbelin001@gmail.com>
2013-03-20 11:48:17 +0100

    Fin des fonctionalités d'attaque (F6 - 7  - 9)

--- src/main/java/fr/univaix/iut/pokebattle/smartcells/PokemonAttakCell.java
+++ src/main/java/fr/univaix/iut/pokebattle/smartcells/PokemonAttakCell.java
@@ -1,6 +1,11 @@
 package fr.univaix.iut.pokebattle.smartcells;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import fr.univaix.iut.pokebattle.Pokemon;
 import fr.univaix.iut.pokebattle.SmartCell;
 import fr.univaix.iut.pokebattle.Tweet;
 
@@ -14,12 +19,26 @@ public class PokemonAttakCell implements SmartCell {
     	
     	String str[] = Tweet.split(" ");
     	String adversaire = str[3];
+    	String DressAdversaire = str[5];
+    	
     	String attack = str[2];
-  
+    	String pokemon = str[0].substring(1);	
+    	String owner;
+    	
+    	
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pokebattle");
+        EntityManager em = emf.createEntityManager();
+
+        Pokemon poke = em.find(Pokemon.class, pokemon);
+        owner = poke.getNomD();
+
     	if (Tweet.contains("attack"))
     	{
-    		return adversaire + " #attack " + attack + "! /cc @" + Name;
+    		if (Name.contains(owner)) { return adversaire + " #attack " + attack + "! /cc @" + Name + " " + DressAdversaire; }
+    		
+    		return "@" + Name + " @" + owner + " is my owner !";  
     	}
+    	
     	return null ;
     	}
 }
