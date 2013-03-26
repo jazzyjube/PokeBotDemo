09a327a7140c36032d859813899682e982a961ef
PowPo <p1103512@squeeze.iut.univ-aix.fr>
2013-03-26 12:10:29 +0100

    Début création juge

--- src/main/java/fr/univaix/iut/pokebattle/bot/JudgeBot.java
+++ src/main/java/fr/univaix/iut/pokebattle/bot/JudgeBot.java
@@ -1,6 +1,13 @@
 package fr.univaix.iut.pokebattle.bot;
 
 import com.google.common.collect.Lists;
+
+import fr.univaix.iut.pokebattle.smartcell.JudgeSoustraitCell;
+import fr.univaix.iut.pokebattle.smartcell.PokemonAnswerCell;
+import fr.univaix.iut.pokebattle.smartcell.PokemonAttakCell;
+import fr.univaix.iut.pokebattle.smartcell.PokemonCatchCell;
+import fr.univaix.iut.pokebattle.smartcell.PokemonInfosCell;
+import fr.univaix.iut.pokebattle.smartcell.PokemonOwnerCell;
 import fr.univaix.iut.pokebattle.smartcell.SmartCell;
 import fr.univaix.iut.pokebattle.twitter.Tweet;
 
@@ -14,7 +21,10 @@ public class JudgeBot implements Bot {
      * List of smartcell the questions go through to
      * find an answer.
      */
-    final List<SmartCell> smartCells = Lists.newArrayList();
+    final SmartCell[] smartCells = new SmartCell[]{
+            new JudgeSoustraitCell()
+
+    };
 
     /**
      * Ask something to Bot, it will respond to you.
---
+++ src/main/java/fr/univaix/iut/pokebattle/smartcell/JudgeSoustraitCell.java
@@ -0,0 +1,14 @@
+package fr.univaix.iut.pokebattle.smartcell;
+
+import twitter4j.TwitterException;
+import fr.univaix.iut.pokebattle.twitter.Tweet;
+
+public class JudgeSoustraitCell implements SmartCell{
+
+	@Override
+	public String ask(Tweet question) throws TwitterException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
