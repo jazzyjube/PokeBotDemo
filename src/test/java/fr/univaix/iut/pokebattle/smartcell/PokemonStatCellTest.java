82394d3c54726381108846da9ed71de1a438c9f1
Kazuia <b1103252@squeeze.iut.univ-aix.fr>
2013-03-26 11:40:01 +0100

    Ajout du Test Stats

---
+++ src/test/java/fr/univaix/iut/pokebattle/smartcell/PokemonStatCellTest.java
@@ -0,0 +1,31 @@
+package fr.univaix.iut.pokebattle.smartcell;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import fr.univaix.iut.pokebattle.twitter.Tweet;
+
+public class PokemonStatCellTest {
+
+    PokemonInfosCell cell = new PokemonInfosCell();
+
+    @Test
+    public void testStatLevel() {
+    	Tweet tweet = new Tweet("maitre_peter", "@sala_meche #stat #level ?");
+        assertEquals("@maitre_peter #level=1", cell.ask(tweet));
+    }
+
+    @Test
+    public void testStatXp() {
+    	Tweet tweet = new Tweet("maitre_peter", "@sala_meche #stat #xp ?");
+        assertEquals("@maitre_peter #XP=0", cell.ask(tweet));
+    }
+    
+    @Test
+    public void testStatPv() {
+    	Tweet tweet = new Tweet("maitre_peter", "@sala_meche #stat #pv ?");
+        assertEquals("@maitre_peter #PV=100", cell.ask(tweet));
+    }
+
+}
